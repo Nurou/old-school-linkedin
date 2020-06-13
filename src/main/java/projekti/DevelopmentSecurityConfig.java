ccbdbfcceb973941818c67e48ff00dd8d1a56a6d
Joel Hassan <joel.nhn@gmail.com>
2020-06-13 12:21:33 +0300

    app properties good

--- src/main/java/projekti/DevelopmentSecurityConfig.java
+++ src/main/java/projekti/DevelopmentSecurityConfig.java
@@ -3,8 +3,7 @@ package projekti;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Profile("!prod")
@@ -13,7 +12,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 public class DevelopmentSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
-  public void configure(WebSecurity sec) throws Exception {
-    sec.ignoring().antMatchers("/**");
+  public void configure(HttpSecurity http) throws Exception {
+    // poistetaan csrf-tarkistus käytöstä h2-konsolin vuoksi
+    http.csrf().disable();
+    // sallitaan framejen käyttö
+    http.headers().frameOptions().sameOrigin();
+
+    http.authorizeRequests().antMatchers("/h2-console", "/h2-console/**").permitAll().antMatchers("/").permitAll()
+        .antMatchers("/users/**").authenticated().antMatchers("/register").permitAll().anyRequest().authenticated();
+    http.formLogin().permitAll();
+
   }
 }
